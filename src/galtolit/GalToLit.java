cd4cade97a791c9959a06615d5a167ebe8fbc324
Aluminium <Aluminium@192.168.0.3>
2014-01-20 19:37:12 -0700

    test

---
+++ src/galtolit/GalToLit.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package galtolit;
+
+/**
+ *
+ * @author Aluminium
+ */
+public class GalToLit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       double gallons; 
+       double liters; 
+       gallons=10; 
+       liters=gallons*3.7854; 
+            System.out.println(gallons + " gallons is " + liters + "liters.");
+   }
+    
+}
